b598fe59f220243e1006bfdce93cbd0920f35749
Louie Villena <louie@fullstack.ph>
2020-02-05 17:03:37 +0800

    Updates

---
+++ precache-manifest.41e5f4c7861e48cf6454d4727335776e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d470b0de0c7bab34624ddc22066f3a3",
+    "url": "/reactjs-videos/index.html"
+  },
+  {
+    "revision": "9b0ee2b002fef5a52f22",
+    "url": "/reactjs-videos/static/css/main.5157b21c.chunk.css"
+  },
+  {
+    "revision": "5d7a45ed4910bbf748f1",
+    "url": "/reactjs-videos/static/js/2.2e496b81.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/reactjs-videos/static/js/2.2e496b81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9b0ee2b002fef5a52f22",
+    "url": "/reactjs-videos/static/js/main.9dc1751d.chunk.js"
+  },
+  {
+    "revision": "76758be3676c0bce1ea2",
+    "url": "/reactjs-videos/static/js/runtime-main.1a7d28c6.js"
+  }
+]);
